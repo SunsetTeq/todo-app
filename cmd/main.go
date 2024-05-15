2f6cb5029389f900d35d8e8bb5b522fa47c76eae
Aleksandr <dotadgager@gmail.com>
2024-05-15 20:04:06 +0300

    добавили файлы миграции и разобрались с запуском бд в docker

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,26 @@ import (
 	"github.com/SunsetTeq/firstRestApiProject/package/handler"
 	"github.com/SunsetTeq/firstRestApiProject/package/repository"
 	"github.com/SunsetTeq/firstRestApiProject/package/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initalizing configs: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(firstRestApiProject.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs") // добавляем директорию конфига
+	viper.SetConfigName("config")  // имя конфига
+	return viper.ReadInConfig()
 }
---
+++ schema/000001_init.up.sql
@@ -0,0 +1,39 @@
+CREATE TABLE  users
+(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    name varchar(255) not null,
+    username varchar(255) not null unique,
+    password_hash varchar(255) not null
+);
+
+CREATE TABLE todo_lists
+(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title varchar(255) not null,
+    description varchar(255)
+);
+
+CREATE TABLE users_lists
+(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id int references users(id) on delete cascade not null,
+    list_id int references todo_lists(id) on delete cascade not null
+);
+
+CREATE TABLE todo_items
+(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title varchar(255) not null,
+    description varchar(255),
+    done boolean not null default false
+);
+
+CREATE TABLE lists_items
+(
+    id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    item_id int references todo_items (id) on delete cascade not null,
+    list_id int references todo_lists (id) on delete cascade not null
+);
+
+
+
