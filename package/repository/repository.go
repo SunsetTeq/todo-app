045c8e7dc3b7a02fce971faceb45e7c40ca3c1d4
Aleksandr <dotadgager@gmail.com>
2024-07-20 18:47:23 +0300

    Реализован функционал PUT (обновления списков)

--- package/repository/repository.go
+++ package/repository/repository.go
@@ -15,6 +15,7 @@ type TodoList interface {
 	GetAll(userId int) ([]firstRestApiProject.TodoList, error)
 	GetById(userId, listId int) (firstRestApiProject.TodoList, error)
 	Delete(userId, listId int) error
+	Update(userId, listId int, input firstRestApiProject.UpdateListInput) error
 }
 
 type TodoItem interface {
--- package/repository/todo_list_postgres.go
+++ package/repository/todo_list_postgres.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/SunsetTeq/firstRestApiProject"
 	"github.com/jmoiron/sqlx"
+	"github.com/sirupsen/logrus"
+	"strings"
 )
 
 type TodoListPostgres struct {
@@ -66,5 +68,35 @@ func (r *TodoListPostgres) Delete(userId, listId int) error {
 
 	_, err := r.db.Exec(query, userId, listId)
 
+	return err
+}
+
+func (r *TodoListPostgres) Update(userId, listId int, input firstRestApiProject.UpdateListInput) error {
+	setValues := make([]string, 0)
+	args := make([]interface{}, 0)
+	argId := 1
+
+	if input.Title != nil {
+		setValues = append(setValues, fmt.Sprintf("title=$%d", argId))
+		args = append(args, *input.Title)
+		argId++
+	}
+
+	if input.Description != nil {
+		setValues = append(setValues, fmt.Sprintf("description=$%d", argId))
+		args = append(args, *input.Description)
+		argId++
+	}
+
+	setQuery := strings.Join(setValues, ", ")
+
+	query := fmt.Sprintf("UPDATE %s tl SET %s FROM %s ul WHERE tl.id = ul.list_id AND ul.list_id=$%d AND ul.user_id=$%d",
+		todoListsTable, setQuery, usersListsTable, argId, argId+1)
+	args = append(args, listId, userId)
+
+	logrus.Debugf("updateQuery: %s", query)
+	logrus.Debugf("args: %s", args)
+
+	_, err := r.db.Exec(query, args...)
 	return err
 }
